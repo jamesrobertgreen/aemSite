c4a4df0012596b802c75288a0b5ab94da3457329
James Green <jamesg@ensemble.com>
2016-11-30 09:45:42 +0000

    Init commit

---
+++ testaemsite/bundle/src/main/java/ensemble/testaemsite/HelloService.java
@@ -0,0 +1,13 @@
+package ensemble.testaemsite;
+
+/**
+ * A simple service interface
+ */
+public interface HelloService {
+    
+    /**
+     * @return the name of the underlying JCR repository implementation
+     */
+    public String getRepositoryName();
+
+}
---
+++ testaemsite/bundle/src/main/java/ensemble/testaemsite/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package ensemble.testaemsite;
+
+import aQute.bnd.annotation.Version;
